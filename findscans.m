236c9a68837663f9496267e13c7a2a72dd73b400
GelSight, Inc <44114954+gelsightinc@users.noreply.github.com>
2019-06-20 11:20:46 -0400

    Updated findscans function.

--- findscans.m
+++ findscans.m
@@ -1,8 +1,21 @@
 function scans = findscans(spath, scannm, tag)
 %FINDSCANS Reads a 3D measurement saved in TMD format.
 %   S = findscans(SPATH) finds all scans within the folder specified by the
-%   string SPATH. The output S is a struct array with paths to the scan.yaml, 
-%   heightmap, and normalmap files. 
+%   string SPATH. 
+%
+%   The output S is a struct array with paths saved in the following fields: 
+%       yamlpath    Path to scan.yaml file
+%       tmdpath     Path to heightmap
+%       nrmpath     Path to normal map
+%       tmdfiles    Struct array of all heightmaps found in scan
+%       nrmfiles    Struct array of all normal maps found in scan
+%
+%   S = findscans(SPATH, SCAN) finds all scans within the folder SPATH with 
+%   names that contain the string SCAN. 
+%
+%   S = findscans(SPATH, SCAN, TAG) sets tmdpath and nrmpath to the TMD file
+%   that contains the string TAG if multiple TMD files are present in the scan
+%   folder. 
 %
 
     if ~exist('scannm','var')
@@ -12,7 +25,7 @@ function scans = findscans(spath, scannm, tag)
         tag = [];
     end
     
-    localfiles = dir(fpath);
+    localfiles = dir(spath);
     scans = [];
     sx    = 1;
     for i = 1 : numel(localfiles)
@@ -21,22 +34,22 @@ function scans = findscans(spath, scannm, tag)
             continue;
         end
 
-        if length(localfiles(i).name) < length(basenm)
+        if length(localfiles(i).name) < length(scannm)
             continue;
         end
 
         % Skip files
-        localpath = fullfile(fpath,localfiles(i).name);
+        localpath = fullfile(spath,localfiles(i).name);
         if ~isdir(localpath)
             continue;
         end
 
-        st = strfind(localfiles(i).name,basenm);
+        st = strfind(localfiles(i).name,scannm);
 
         scanpath = fullfile(localpath,'scan.yaml');
-        % if the basenm is empty, then we save the scan
-        % otherwise, we only save scans that match basenm
-        if exist(scanpath,'file') && (isempty(basenm) || ~isempty(st))
+        % if the scannm is empty, then we save the scan
+        % otherwise, we only save scans that match scannm
+        if exist(scanpath,'file') && (isempty(scannm) || ~isempty(st))
             scans(sx).yamlpath = scanpath;
             scans(sx).tmdpath = '';
             scans(sx).nrmpath = '';
@@ -58,109 +71,85 @@ function scans = findscans(spath, scannm, tag)
     end
     
     % Find 3D files within matching scans
+    for i = 1 : numel(scans)
+        if isempty(scans(i).tmdpath)
+            scans(i) = add3dfiles(scans(i).yamlpath, tag);
+        end
+    end
 
 end
 
 %
 %
 %
-function files = get3dfiles(fpath, basenm, tag)
+function sdata = add3dfiles(yamlpath, tag)
 
-    if ~exist('tag','var')
-        tag = [];
-    end
+    sdata.yamlpath = yamlpath;
+    sdata.tmdpath = '';
+    sdata.nrmpath = '';
+    sdata.tmdfiles = [];
+    sdata.nrmfiles = [];
+
+    [fpath,scanfile] = fileparts(yamlpath);
     
-    allfiles = dir(fpath);
-    ix = 1;
-    for i = 1 : numel(allfiles)
-        if length(allfiles(i).name) < length(basenm)
-            continue;
-        end
+    localfiles = dir(fpath);
+    tmdfiles = [];
+    tmdix = 1;
+    tmdpath = '';
+    for i = 1 : numel(localfiles)
         
-        st = strfind(allfiles(i).name,basenm);
-        if isempty(st) || st ~= 1
-            continue;
-        end
+        [tempdr,tempnm,tempex] = fileparts(localfiles(i).name);
         
-        dpath = fullfile(fpath,allfiles(i).name);
-        if ~isdir(dpath)
-            continue;
+        if strcmp(tempex,'.tmd')
+            tmdfiles(tmdix).path = fullfile(fpath,localfiles(i).name);
+            tmdix = tmdix + 1;
         end
-        
-        localfiles = dir(dpath);
-        tmdpath = '';
-        images = [];
-        tmdfiles = [];
-        tmdix = 1;
-        for j = 1 : numel(localfiles)
-            [tempdr,tempnm,tempex] = fileparts(localfiles(j).name);
-            
-            if strcmp(localfiles(j).name,'scan.yaml')
-                %thisscan = ReadYaml(fullfile(dpath,localfiles(j).name));
-                thisscan = readscanfile(fullfile(dpath,localfiles(j).name));
-                for imix = 1 : numel(thisscan.images)
-                    images(imix).path = fullfile(dpath,thisscan.images(imix).path);
-                end
-            end
-            
-            if strcmp(tempex,'.tmd')
-                tmdfiles(tmdix).path = fullfile(dpath,localfiles(j).name);
-                
-                tmdix = tmdix + 1;
-            end
 
-            sx = strfind(localfiles(j).name,tag);
-            if strcmp(tempex,'.tmd') && ~isempty(sx)
-                tmdpath = fullfile(fpath,allfiles(i).name,localfiles(j).name);
-            end
-                        
+        sx = strfind(localfiles(i).name,tag);
+        if strcmp(tempex,'.tmd') && ~isempty(sx)
+            tmdpath = fullfile(fpath,localfiles(i).name);
         end
-        
-        if ~isempty(tmdfiles) && length(tmdpath) == 0
-            tmdpath = tmdfiles(1).path;
-        end
-        
-        if length(tmdpath) == 0 && isempty(images)
-            continue;
-        end
-        
-        if isempty(tmdpath) && ~isempty(tag)
-            warning('no TMD file found with tag = %s',tag);
-        end
-        
-        if numel(tmdfiles) == 1
-            tmdpath = tmdfiles(1).path;
-        end
-        
-        if numel(tmdfiles) > 1 && isempty(tag)
-            warning('%d TMD files found in %s, using first',numel(tmdfiles),dpath);
-            tmdpath = tmdfiles(1).path;
-        end
-        
-        % Check for normal maps with same names as TMD files
-        [tmdparent,tmdname,tmdext] = fileparts(tmdpath);
-        nrmpath = fullfile(tmdparent,[tmdname '_nrm.png']);
-        if ~exist(nrmpath,'file')
-            nrmpath = '';
-        end
-        
-        for tmdix = 1 : numel(tmdfiles)
-            [tmdparent,tmdname,tmdext] = fileparts(tmdfiles(tmdix).path);
-            localnrmpath = fullfile(tmdparent,[tmdname '_nrm.png']);
-            if ~exist(localnrmpath,'file')
-                localnrmpath = '';
-            end 
-            nrmfiles(tmdix).path = localnrmpath;
-        end
-        
-        files(ix).scanpath = fullfile(dpath,'scan.yaml');
-        files(ix).tmdpath  = tmdpath;
-        files(ix).nrmpath  = nrmpath;
-        files(ix).tmdfiles = tmdfiles;
-        files(ix).nrmfiles = nrmfiles;
-        files(ix).images   = images;
-        ix = ix + 1;
+                    
     end
+        
+    if ~isempty(tmdfiles) && length(tmdpath) == 0
+        tmdpath = tmdfiles(1).path;
+    end
+        
+    if isempty(tmdpath) && ~isempty(tag)
+        warning('no TMD file found with tag = %s',tag);
+    end
+        
+    if numel(tmdfiles) == 1
+        tmdpath = tmdfiles(1).path;
+    end
+        
+    if numel(tmdfiles) > 1 && isempty(tag)
+        warning('%d TMD files found in %s, using first',numel(tmdfiles),dpath);
+        tmdpath = tmdfiles(1).path;
+    end
+        
+    % Check for normal maps with same names as TMD files
+    [tmdparent,tmdname,tmdext] = fileparts(tmdpath);
+    nrmpath = fullfile(tmdparent,[tmdname '_nrm.png']);
+    if ~exist(nrmpath,'file')
+        nrmpath = '';
+    end
+        
+    nrmfiles = [];
+    for tmdix = 1 : numel(tmdfiles)
+        [tmdparent,tmdname,tmdext] = fileparts(tmdfiles(tmdix).path);
+        localnrmpath = fullfile(tmdparent,[tmdname '_nrm.png']);
+        if ~exist(localnrmpath,'file')
+            localnrmpath = '';
+        end 
+        nrmfiles(tmdix).path = localnrmpath;
+    end
+        
+    sdata.tmdpath  = tmdpath;
+    sdata.nrmpath  = nrmpath;
+    sdata.tmdfiles = tmdfiles;
+    sdata.nrmfiles = nrmfiles;
 
 end
 
